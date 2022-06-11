81162e45e4011686e9fff92e24a623de212a6618
izvergoleg <izvergoleg@gmail.com>
2022-06-11 16:34:46 +0300

    folders structure changes

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,6 +1,6 @@
 # Наличие этого файла в текущей папке указывает Питону, что эта папка (gui) является не просто папкой, а пакетом gui
 #Этот файл может быть пустым
 
-from .main import Ui_MainWindow # для удобства к обращению из Qtable.py к Ui_MainWindow,  Ui_MainWindow в gui/main.py
+from .MainWindow import Ui_MainWindow # для удобства к обращению из Qtable.py к Ui_MainWindow,  Ui_MainWindow в gui/main.py
 
 from .dialog import Ui_Dialog
